49a896605baab96bd66a39f7a4186cb0b1756a92
Posbon <89262281906@mail.ru>
2020-03-30 21:27:07 +0300

    final

--- src/test/java/TestAlfaCard.java
+++ src/test/java/TestAlfaCard.java
@@ -1,40 +1,21 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestAlfaCard {
 
-    WebDriver webDriver;
-
-    @BeforeAll
-    public static void setupDriverPath() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-    }
-
-    @BeforeEach
-    public void setUp() {
-        webDriver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        webDriver.quit();
-    }
-
     @Test
     void startService() {
-        webDriver.get("http://localhost:9999/");
-        List<WebElement> inputElements = webDriver.findElements(By.className("input__control"));
-        inputElements.get(0).sendKeys("Иннокентий");
-        inputElements.get(1).sendKeys("+70001112233");
-        webDriver.findElement(By.className("checkbox__box")).click();
-        webDriver.findElement(By.tagName("button")).click();
-        String actualText = webDriver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
-        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText.trim());
+        open("http://localhost:9999");
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Иннокентий");
+        form.$("[data-test-id=phone] input").setValue("+70001112233");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
 
